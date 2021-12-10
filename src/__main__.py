1c7133627e8a99cfcd3da6d1af713f3273279c4a
Aleks4920 <49626050+Aleks4920@users.noreply.github.com>
2021-12-10 13:55:54 -0500

    added repeat function and comments 	also took away print statment

--- src/__main__.py
+++ src/__main__.py
@@ -9,27 +9,34 @@ def main():
     bot = Bot(command_prefix='!')
 
 
-
-
+    #print bot info when it boots up
     @bot.event
     async def on_ready():
         print('Logged in as')
         print(bot.user.name)
         print(bot.user.id)
         print('------')
+     
+    #Confirm that bot is connected to server    
+    @bot.event
+    async def on_connect():
+        print("--Connected to server--")
 
-
-                    
+    #Coin Flip function
     @bot.command(pass_context=True)
     async def coinflip(ctx):
         rand = random.choice([0,1])
-        print(rand)
         if rand == 0:
             await ctx.channel.send('Heads!')
         elif rand == 1:
             await ctx.channel.send('Tails!')
 
+    # print message back
+    @bot.command()
+    async def repeat(ctx, *, arg):
+        await ctx.send(arg)
 
+    #Run bot
     bot.run(secrets.secretToken)
 
     
