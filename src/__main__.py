8365f13205249c7f36e4c16fb23158268f520d4c
Paul Molczanski <paulmski@gmail.com>
2021-12-11 11:31:10 -0500

    Started basic announce command, and way to configure which channel the announcement goes too.

--- src/__main__.py
+++ src/__main__.py
@@ -2,6 +2,7 @@
 
 def main():
     from discord.ext.commands import Bot
+    import discord.utils
     import random
     import secrets
 
@@ -30,6 +31,20 @@ def main():
             await ctx.channel.send('Heads!')
         elif rand == 1:
             await ctx.channel.send('Tails!')
+
+    # Command to make an announcement to whole server, but in a specific channel.
+    @bot.command(pass_context=True)
+    async def announce(ctx, subCommand: str, subSubCommand: str, arg: str):
+        # Used to configure settings related to this command.
+        if (subCommand == 'config'):
+            
+            # Used to configure which channel the bot should make announcements too.
+            if (subSubCommand == 'channel'):
+                # TODO:(Allow for the ability to modify channel for announcement)
+                channel = discord.utils.get(ctx.guild.channels, name=arg)
+                await channel.send('This is the new announcement channel.')
+
+
 
     # print message back
     @bot.command()
