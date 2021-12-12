2979b85509b0f671a9b53e820d96651b50201e48
tuanyvan <tuanyvan747@gmail.com>
2021-12-12 00:55:59 -0500

    Added helpful comment to running bot.

--- src/__main__.py
+++ src/__main__.py
@@ -25,12 +25,12 @@ def main():
     # Flip a coin and tell the user what the result was.
     @bot.command(pass_context=True)
     async def coinflip(ctx):
-        rand = random.choice([0,6000])
         '''
         According to Teare and Murray's "Probability of a tossed coin landing on edge", extrapolations from a physical simulation model
         suggests "that the probability of an American nickel landing on edge is approximately 1 in 6000 tosses."
         Murray, Daniel B., and Scott W. Teare. “Probability of a Tossed Coin Landing on Edge.” Physical Review E, vol. 48, no. 4, 1993, pp. 2547–2552., https://doi.org/10.1103/physreve.48.2547. 
         '''
+        rand = random.choice([0,6000])
         if rand == 420:
             responses = ['Holy cow, it landed on it\'s side!', 'You won\'t believe this but it landed on its side!', 'Despite all odds, it landed on it\'s side!']
             random_response = random.choice([0, len(responses)])
@@ -52,13 +52,15 @@ def main():
                 channel = discord.utils.get(ctx.guild.channels, name=arg)
                 await channel.send('This is the new announcement channel.')
 
-    # print message back
+    # Print the message back.
     @bot.command()
     async def repeat(ctx, *, arg):
         await ctx.send(arg)
 
-    #Run bot
-    bot.run(secrets.secretToken)
+    # Run the bot using the secretToken parameter from secrets.py.
+    # For devs running their own version of the bot, create a secrets.py file to the src directory, and put the DISCORD_TOKEN as a variable.
+    # Remember not to add the secrets.env file when comitting/pushing.
+    bot.run(secrets.DISCORD_TOKEN)
 
 if __name__ == '__main__':
   main()
