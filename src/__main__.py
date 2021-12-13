09c57b2a190d499cad59a1a0a3f80737c0b1a65e
tuanyvan <tuanyvan747@gmail.com>
2021-12-13 14:41:56 -0500

    Edited code comments.

--- src/__main__.py
+++ src/__main__.py
@@ -29,19 +29,19 @@ def main():
     ANNOUNCEMENT_CHANNEL = os.getenv("ANNOUNCEMENT_CHANNEL")
 
     # Establish a connection to the Google Sheets API.
-    creds = None
     # The file token.json stores the user's access and refresh tokens, and is created automatically when the authorization flow completes for the first time.
+    creds = None
     if os.path.exists('token.json'):
         creds = Credentials.from_authorized_user_file('token.json', SCOPES)
 
-    # If there are no (valid) credentials available, let the user log in.
+    # If token.json does not exist, log in manually.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run.
+        # Save the credentials in token.json for the next run.
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
 
