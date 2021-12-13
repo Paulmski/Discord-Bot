b661966ef3bfe68820ce7c4b6466fd5e1597aece
tuanyvan <tuanyvan747@gmail.com>
2021-12-13 11:48:16 -0500

    Added .json to .gitignore, edited Google Sheets API.

--- src/__main__.py
+++ src/__main__.py
@@ -22,7 +22,27 @@ def main():
 
     # Getting environment variables.
     SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+    RANGE_NAME = os.getenv("RANGE_NAME")
     DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('sheets', 'v4', credentials=creds)
 
     bot = Bot(command_prefix='!')
 
