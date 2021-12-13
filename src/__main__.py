16780acc15787dfca62149dbc509736943d7aed7
tuanyvan <tuanyvan747@gmail.com>
2021-12-13 14:44:55 -0500

    Edited function call comments.

--- src/__main__.py
+++ src/__main__.py
@@ -69,11 +69,11 @@ def main():
         def __init__(self):
             self.fetch_due_dates.start()
 
-        # Declare a function to unload fetch_due_date cog.
+        # Declare a function to unload the fetch_due_date cog.
         def cog_unload(self):
             self.fetch_due_dates.cancel()
 
-        # fetch_due_dates loop.
+        # Declare the fetch_due_dates loop.
         @tasks.loop(minutes=1.0)
         async def fetch_due_dates(self):
             print("Fetching due dates...")
@@ -179,8 +179,7 @@ def main():
     @bot.command(pass_context=True)
     async def coinflip(ctx):
         '''
-        According to Teare and Murray's "Probability of a tossed coin landing on edge", extrapolations from a physical simulation model
-        suggests "that the probability of an American nickel landing on edge is approximately 1 in 6000 tosses."
+        According to Teare and Murray's "Probability of a tossed coin landing on edge", extrapolations from a physical simulation model suggests "that the probability of an American nickel landing on edge is approximately 1 in 6000 tosses."
         Murray, Daniel B., and Scott W. Teare. “Probability of a Tossed Coin Landing on Edge.” Physical Review E, vol. 48, no. 4, 1993, pp. 2547–2552., https://doi.org/10.1103/physreve.48.2547. 
         '''
         rand = random.randint(0, 6000)
@@ -198,10 +197,9 @@ def main():
     async def announce(ctx, subCommand: str, subSubCommand: str, arg: str):
         # Used to configure settings related to this command.
         if (subCommand == 'config'):
-            
             # Used to configure which channel the bot should make announcements too.
             if (subSubCommand == 'channel'):
-                # TODO:(Allow for the ability to modify channel for announcement)
+                # TODO: Allow for the ability to modify channel for announcement.
                 channel = discord.utils.get(ctx.guild.channels, name=arg)
                 await channel.send('This is the new announcement channel.')
 
