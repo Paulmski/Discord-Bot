a09a583a1cc58ff09043f06b0d133e4fd77d842f
tuanyvan <tuanyvan747@gmail.com>
2021-12-23 12:35:11 -0500

    Rewrote code to conform with classes and OOP.

--- src/sheets_parser.py
+++ src/sheets_parser.py
@@ -3,6 +3,8 @@
 import logging
 from time import strftime
 from datetime import datetime, timedelta
+from classes.Assignment import Assignment
+from classes.Course import Course
 
 # Returns a dictionary containing assignment information.
 def fetch_due_dates(service, SPREADSHEET_ID=None, RANGE_NAME=None):
@@ -27,10 +29,11 @@ def fetch_due_dates(service, SPREADSHEET_ID=None, RANGE_NAME=None):
 
         # Grab the indexes of the headers from A1:E1.
         index = {
-            "course": header.index("Course Name"),
-            "assignment": header.index("Assignment Name"),
-            "due_date": header.index("Due Date"),
-            "days_until": header.index("Days Until Due Date"),
+            "code": header.index("Code"),
+            "course": header.index("Course"),
+            "assignment": header.index("Assignment"),
+            "due_date": header.index("Due"),
+            "days_left": header.index("Days Left"),
             "notes": header.index("Notes")
         }
 
@@ -40,28 +43,17 @@ def fetch_due_dates(service, SPREADSHEET_ID=None, RANGE_NAME=None):
         for row in values[1:]:
             # Should there be no IndexError raised...
             try:
-                # If the class name has changed from the A column, change the current_class variable.
+
+                # If the class name has changed from the A column, change the current course.
                 if row[index["course"]] != "":
-                    course = row[index["course"]]
+                    c_course = row[index["course"]]
+                    assignments[c_course] = []
 
-                # Assign the assignment name, due date, and days until due date.
-                assignment = row[index["assignment"]]
-                due_date = row[index["due_date"]]
-                days_left = row[index["days_until"]]
+                c_asgnmt = Assignment()
+                c_asgnmt.parse_values(row, index)
 
-                # If there are notes in this row, assign the value to notes.
-                if len(row) == 5:
-                    notes = row[index["notes"]]
-
-                # Otherwise, just assign it as a blank value.
-                else:
-                    notes = ""
-
-                # If the assignment is due in a week, add it to the final message to @everyone.
-                if int(days_left) >= 0 and int(days_left) <= 7:
-                    if course not in assignments.keys():
-                        assignments[course] = []
-                    assignments[course].append([assignment, due_date, days_left, notes])
+                if c_asgnmt.days_left >= 0 and c_asgnmt.days_left <= 7:
+                    assignments[c_course].append([c_asgnmt.name, c_asgnmt.due.strftime("%B %d"), c_asgnmt.days_left, c_asgnmt.note])
 
             # Otherwise, pass.
             except IndexError:
@@ -89,39 +81,35 @@ def get_daily_schedule(service, SPREADSHEET_ID=None, COURSE_SHEET=None):
 
     # Grab the indexes of the headers from A1:E1.
     index = {
-        "course": header.index("Course Name"),
+        "code": header.index("Code"),
+        "name": header.index("Name"),
         "day": header.index("Day"),
-        "time": header.index("Time"),
+        "start_time": header.index("Time"),
         "end_time": header.index("Ends"),
-        "room": header.index("Room")
+        "room": header.index("Room"),
+        "status": header.index("Status")
     }
 
     for row in values[1:]:
         if row[index["day"]] == current_day:
 
-            # Convert EST times to GMT times.
-            # Timezone translation UTC-05:00 to UTC+00:00.
-            gmt_start_time = datetime.strptime(now.strftime(f"%Y-%m-%dT") + row[index['time']], "%Y-%m-%dT%H:%M") + timedelta(hours=5)
+            c_class = Course()
+            c_class.parse_state(row, index)
 
             # Only returns events that are in the future.
-            if gmt_start_time < datetime.now():
+            # Convert c_class GMT state to EST (GMT-05:00)
+            if c_class.start_time + timedelta(hours=-5) < now:
                 continue
-            gmt_end_time = datetime.strptime(now.strftime(f"%Y-%m-%dT") + row[index['end_time']], "%Y-%m-%dT%H:%M") + timedelta(hours=5)
-            
-            # 12-hour time conversions.
-            th_start_time = datetime.strptime(row[index["time"]], "%H:%M").strftime("%I:%M%p")
-            th_end_time = datetime.strptime(row[index["end_time"]], "%H:%M").strftime("%I:%M%p")
-            
             
             events.append(
                 {
                     "entity_type": 3, # Value 3 is EXTERNAL events.
-                    "entity_metadata": { "location": f"Room {row[index['room']]}" },
-                    "name": row[index["course"]],
+                    "entity_metadata": { "location": f"Room {c_class.room}" },
+                    "name": f"{c_class.code} - {c_class.name}",
                     "privacy_level": 2, # Required value as per documentation.
-                    "scheduled_start_time": str(gmt_start_time),
-                    "scheduled_end_time": str(gmt_end_time),
-                    "description": f"{row[index['course']]} will take place on {gmt_start_time.strftime('%B %d')} from {th_start_time} to {th_end_time} in Room {row[index['room']]}."
+                    "scheduled_start_time": str(c_class.start_time),
+                    "scheduled_end_time": str(c_class.end_time),
+                    "description": c_class.description
                 }
             )
 
