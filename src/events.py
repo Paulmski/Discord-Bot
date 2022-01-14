0a1516acbed45ddad46c700ae72a0ab75342f970
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-01-13 23:02:24 -0500

    Enforcing single quote use where necessary

--- src/events.py
+++ src/events.py
@@ -10,12 +10,12 @@ from dotenv import load_dotenv
 from classes.Assignment import Assignment
 
 load_dotenv()
-SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
-RANGE_NAME = os.getenv("RANGE_NAME")
-COURSE_SHEET = os.getenv("COURSE_SHEET")
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-ANNOUNCEMENT_CHANNEL = os.getenv("ANNOUNCEMENT_CHANNEL")
-GUILD_ID = os.getenv("GUILD_ID")
+SPREADSHEET_ID = os.getenv('SPREADSHEET_ID')
+RANGE_NAME = os.getenv('RANGE_NAME')
+COURSE_SHEET = os.getenv('COURSE_SHEET')
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+ANNOUNCEMENT_CHANNEL = os.getenv('ANNOUNCEMENT_CHANNEL')
+GUILD_ID = os.getenv('GUILD_ID')
 
 # Declare the FetchDate class, inheriting methods from Cog.
 class FetchDate(commands.Cog):
@@ -33,7 +33,7 @@ class FetchDate(commands.Cog):
     async def fetch_due_dates(self, channel_id=None):
         if (datetime.now().hour != 6 and channel_id == None):
             return
-        logging.info("Fetching due dates...")
+        logging.info('Fetching due dates...')
 
         # Pass Sheets service and metadata to sheets_parser.fetch_due_dates()
         assignments = sheets_parser.fetch_assignments(self.service, SPREADSHEET_ID, RANGE_NAME)
@@ -45,19 +45,19 @@ class FetchDate(commands.Cog):
 
         # Make a call to the @everyone event handler with the assignments array passed as an argument.
         if final_assignments != []:
-            await self.announce_assignments(final_assignments, title="Due Dates For Today", channel_id=channel_id)
+            await self.announce_assignments(final_assignments, title='Due Dates For Today', channel_id=channel_id)
         elif channel_id != None:
             channel = self.bot.get_channel(channel_id)
             await channel.send('Looks like there\'s no assignments in the next 14 days, you can relax... for now.')
         
     @fetch_due_dates.before_loop
     async def before_fetch(self):
-        logging.debug("Initiating data fetching.")
+        logging.debug('Initiating data fetching.')
     
     
     # This function must be passed an array of assignments in which each assignment has its course code and course name
     async def announce_assignments(self, due_dates, title: str, channel_id=None):
-        """Sends a Discord message with assignment due dates based on a Context channel or Announcements channel ID in .env."""
+        '''Sends a Discord message with assignment due dates based on a Context channel or Announcements channel ID in .env.'''
 
         # Instantiate the Embed.
         embedded_message = discord.Embed(title=title, colour=discord.Colour.from_rgb(160, 165, 25))
@@ -69,14 +69,14 @@ class FetchDate(commands.Cog):
             channel = self.bot.get_channel(channel_id)
         elif self.bot.get_channel(int(ANNOUNCEMENT_CHANNEL)) != None:
             channel = self.bot.get_channel(int(ANNOUNCEMENT_CHANNEL))
-        elif discord.utils.get(self.bot.get_all_channels(), name="announcements") != None:
-            channel = discord.utils.get(self.bot.get_all_channels(), name="announcements")
+        elif discord.utils.get(self.bot.get_all_channels(), name='announcements') != None:
+            channel = discord.utils.get(self.bot.get_all_channels(), name='announcements')
         else:
-            logging.error("Unable to find channel to send announcement to.")
+            logging.error('Unable to find channel to send announcement to.')
             return
           
         # For every course in the due date list...
-        course_assignments = ""
+        course_assignments = ''
         current_code = due_dates[0].code
         current_course_name = due_dates[0].course_name
 
@@ -88,43 +88,45 @@ class FetchDate(commands.Cog):
 
             # Default case when the assignment course differs from the previous one.
             else:
-                embedded_message.add_field(name=f"__{current_code} - {current_course_name}__", value=course_assignments + "", inline=False)
+                embedded_message.add_field(name=f'__{current_code} - {current_course_name}__', value=course_assignments + '', inline=False)
                 course_assignments = self.format_assignment(assignment)
                 current_code = assignment.code
                 current_course_name = assignment.course_name
 
 
         # Add the last field with the remaining course assignments.
-        embedded_message.add_field(name=f"__{current_code} - {current_course_name}__", value=course_assignments + "", inline=False)
+        embedded_message.add_field(name=f'__{current_code} - {current_course_name}__', value=course_assignments + '', inline=False)
         
         # Add project information to bottom.
-        embedded_message.add_field(name="\n\nAbout Me", value="I am part of the Lakehead CS 2021 Guild's Discord-Bot project! [Contributions on GitHub are welcome!](https://github.com/Paulmski/Discord-Bot/blob/main/CONTRIBUTING.md)")
+        embedded_message.add_field(name='\n\nAbout Me', value='I am part of the Lakehead CS 2021 Guild\'s Discord-Bot project! [Contributions on GitHub are welcome!](https://github.com/Paulmski/Discord-Bot/blob/main/CONTRIBUTING.md)')
     
         # Send the message to the announcements channel.
-        await channel.send("", embed=embedded_message, delete_after=23*60*60)
+        await channel.send('', embed=embedded_message, delete_after=23*60*60)
 
     def format_assignment(self, assignment: Assignment):
-        """Formats an Assignment object to a string that will be displayed in a Discord Embed."""
+        '''
+        Formats an Assignment object to a string that will be displayed in a Discord Embed.
+        '''
         # Parse the information from the assignment list.
         name = assignment.name
-        due_date = assignment.due.strftime("%A, %B %d")
+        due_date = assignment.due.strftime('%A, %B %d')
         days_left = assignment.days_left
 
         # Change days_left to a different code block color depending on days left.
         if days_left > 3:
-            days_left = f"```diff\n+ {days_left} days remaining.```"
+            days_left = f'```diff\n+ {days_left} days remaining.```'
         elif days_left > 0:
-            days_left = f"```fix\n {days_left} days remaining.```"
+            days_left = f'```fix\n {days_left} days remaining.```'
         else:
-            days_left = f"```diff\n- {days_left} days remaining.```"
+            days_left = f'```diff\n- {days_left} days remaining.```'
 
         notes = assignment.note
 
         # Append the information to the course_assignments.
-        if notes == "":
-            return f"\n**{name}**\nDue on {due_date}, {datetime.now().year}.\n{days_left}\n"
+        if notes == '':
+            return f'\n**{name}**\nDue on {due_date}, {datetime.now().year}.\n{days_left}\n'
         else:
-            return f"\n**{name}**\nDue on {due_date}, {datetime.now().year}.\n{days_left}__Notes:__\n{notes}\n"
+            return f'\n**{name}**\nDue on {due_date}, {datetime.now().year}.\n{days_left}__Notes:__\n{notes}\n'
 
 # Declare EventScheduler Cog.
 class EventScheduler(commands.Cog):
@@ -150,18 +152,18 @@ class EventScheduler(commands.Cog):
         await self.bot.wait_until_ready() # Bot needs to wait until ready, especially on the first iteration.
         # Set the class' guild state (bot.get_guild() returns a Guild object)
         guild = self.bot.get_guild(int(GUILD_ID))
-        logging.info(f"Scheduling to server {guild.name}.")
+        logging.info(f'Scheduling to server {guild.name}.')
 
         # Get dictionary of daily event JSON payloads from sheets_parser.get_daily_schedule().
         schedule = sheets_parser.fetch_courses(self.service, SPREADSHEET_ID, COURSE_SHEET)
 
         if schedule == []:
-            logging.info("No events were scheduled.")
+            logging.info('No events were scheduled.')
         # Post events using HTTP.
-        route = Route("POST", f"/guilds/{GUILD_ID}/scheduled-events", guild_id=GUILD_ID)
+        route = Route('POST', f'/guilds/{GUILD_ID}/scheduled-events', guild_id=GUILD_ID)
 
         now = datetime.now() + timedelta(hours=5)
-        current_day = now.strftime("%A") # Formatted for weekday's full name.
+        current_day = now.strftime('%A') # Formatted for weekday's full name.
         for course in schedule:
             if course.day == current_day and course.start_time > now:
                 event = course.to_json_event()
@@ -201,4 +203,4 @@ class EventScheduler(commands.Cog):
   
     @schedule_events.before_loop
     async def before_scheduling(self):
-        logging.debug("Initiating event scheduler.")
+        logging.debug('Initiating event scheduler.')
